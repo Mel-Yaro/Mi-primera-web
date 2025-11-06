2baeb9ffe17a0c2cf9a82aad56ccd22818e03d7c
Mel-Yaro <145498433+Mel-Yaro@users.noreply.github.com>
2025-11-06 18:29:22 +0000

    Intento de generador

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 const titulo = document.getElementById("titulo");
 
-console.log(titulo);
+let estadoInicial = true;
+let posicionTexto = 0;
+let posicionProfecia = 0;
+const textos = ["Este payaso hace de todo", "Adivino dizfrazado", "Este payaso se las inventa todas", "Adivino enmascarado","hace de todo", "payaso multifuncional"];
+const textosProfecia = ["La risa es el mejor remedio", "El humor es la clave", "La alegría es contagiosa", "La diversión está asegurada", "La sorpresa es parte del show", "La magia está en el aire"];
+
+profecia.innerText = textosProfecia[posicionProfecia];
+
+textoInicial.innerText = textos[posicionTexto];
+
+textoInicial.addEventListener("click", () => {
+    posicionTexto++;
+   textoInicial.innerText = textos[posicionTexto];
+    if (posicionTexto === textos.length-1) {
+       posicionTexto = 0;
+   }
+});
+
+profecia.addEventListener("click", () => {
+    posicionProfecia++;
+   profecia.innerText = textosProfecia[posicionProfecia];
+    if (posicionProfecia === textosProfecia.length-1) {
+       posicionProfecia = 0;
+   }
+});
 
 titulo.addEventListener("click", () => {
+    if (estadoInicial === true) {
     titulo.innerText= "Este payaso lo sabe todo";
+    
+} else {
+    titulo.innerText= "Adivino en la fiesta";
+}
+estadoInicial = !estadoInicial;
 });
+
+
+
+
+
+
+
+
